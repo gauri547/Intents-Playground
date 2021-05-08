e290c0ca997a3fd8bf810d1f33301f9bb97bfbd3
Gauri Tanwar <gauritanwar2507@gmail.com>
2021-05-08 19:42:27 +0530

    updated task 5.3 !!

--- app/src/main/java/com/streamliners/intentsplayground/Const.java
+++ app/src/main/java/com/streamliners/intentsplayground/Const.java
@@ -6,4 +6,11 @@ public class Const {
     FINAL_DATA="finalData",
     MIN_VALUE = "minValue",
     MAX_VALUE ="maxValue";
+    public static final String DATA = "data";
+    public static final String RADIO_BUTTON_CHECK = "radioButton";
+    public static final String EDT_INITIAL_COUNT_VALUE = "editTextValue";
+    public static String COUNT_VAlUE="countValue";
+
+
+
 }
--- app/src/main/java/com/streamliners/intentsplayground/MainActivity.java
+++ app/src/main/java/com/streamliners/intentsplayground/MainActivity.java
@@ -1,41 +1,76 @@
 
 package com.streamliners.intentsplayground;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.intentsplayground.databinding.ActivityMainBinding;
+import com.streamliners.intentsplayground.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
     ActivityMainBinding b;
     int qty = 0;
     private int minValue, maxValue;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Initialise
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+
+        //event handler
         eventHandler();
+        //Receive data
         receiveData();
 
+        //send data back
         sendDataBack();
+
+        saveData(savedInstanceState);
+
+    }
+
+    /**
+     * Save data in sharedPreference and savedInstanceState
+     *
+     * @param savedInstanceState Data save on configuration changes
+     */
+    private void saveData(Bundle savedInstanceState) {
+        //check bundle
+        if (savedInstanceState != null) {
+            qty = savedInstanceState.getInt(Const.COUNT_VAlUE);
+        } else {
+            qty = sharedPreferences.getInt(Const.COUNT_VAlUE, 0);
+        }
+
+        b.qty.setText(qty + "");
     }
 
 
+    /**
+     * Send data back to IntentPlayground Activity
+     */
     private void sendDataBack() {
+        //click event handler
         b.btnSendBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //Check quantity
                 if (qty >= minValue && qty <= maxValue) {
-
+                    //Send data back
                     Intent replyIntent = new Intent(MainActivity.this, IntentPlaygroundActivity.class);
-                    replyIntent.putExtra(Constants.FINAL_DATA, qty);
+                    replyIntent.putExtra(Const.FINAL_DATA, qty);
 
                     setResult(RESULT_OK, replyIntent);
                     finish();
@@ -45,32 +80,44 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
+    /**
+     * Receive data from IntentPlayground Activity
+     */
     private void receiveData() {
 
-
         // check bundle is not null
-        if (getIntent().getExtras() != null) {
-            minValue = getIntent().getExtras().getInt(Constants.MIN_VALUE, 0);
-            maxValue = getIntent().getExtras().getInt(Constants.MAX_VALUE, 0);
-
-            qty = getIntent().getExtras().getInt(Constants.INITIAL_DATA, Integer.MIN_VALUE);
-
-            b.btnSendBack.setVisibility(View.VISIBLE);
-            b.qty.setText(qty + "");
-
+        if (getIntent().getExtras() == null) {
+            sharedPreferences.edit().putInt(Const.COUNT_VAlUE, 0).apply();
+            return;
         }
+        minValue = getIntent().getExtras().getInt(Const.MIN_VALUE, 0);
+        maxValue = getIntent().getExtras().getInt(Const.MAX_VALUE, 0);
+
+        qty = getIntent().getExtras().getInt(Const.INITIAL_DATA, Integer.MIN_VALUE);
+
+
+        b.btnSendBack.setVisibility(View.VISIBLE);
+
+
+        //Edit sharedPreferences
+        sharedPreferences.edit().putInt(Const.COUNT_VAlUE, qty).apply();
     }
 
+    /**
+     * Trigger Event handlers to listen the actions
+     */
     private void eventHandler() {
+        //click event handler on Decrease button
         b.decBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 decreaseQuantity();
             }
         });
-
+        //click event handler on Increase
         b.incBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,19 +126,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //To increase the quantity
+    /**
+     * To increase the quantity
+     */
     private void increaseQuantity() {
+        //update Quantity TextView
         b.qty.setText(++qty + "");
     }
 
-    //To decrease the quantity
+    /**
+     * To decrease the quantity
+     */
     private void decreaseQuantity() {
-
+        // check quantity
         if (qty == 0) {
             Toast.makeText(this, "Quantity is already 0", Toast.LENGTH_SHORT).show();
             return;
         }
-
+        //update Quantity TextView
         b.qty.setText(--qty + "");
+    }
+
+    /**
+     * Save data
+     * @param outState Save data on configuration changes
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(Const.COUNT_VAlUE, qty);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //update sharedPreference
+        sharedPreferences.edit().
+                putInt(Const.COUNT_VAlUE, qty)
+                .apply();
     }
 }
